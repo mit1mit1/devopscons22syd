eda4e80e75ca5efdf75458614a2b7ac99ad9e176
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2024-04-02 14:40:52 +1100

    feat: markdown to comic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import IntroComic from './comics/IntroComic.vue';
 import SupplyChainVulnerabilities from './comics/SupplyChainVulnerabilities.vue';
+import SoftwareArchitectureOverview from './comics/SoftwareArchitectureOverview.vue';
 import { getComicNumber } from './vue-comic-components/utils/comicNumber';
 import DoraReport2021 from './comics/DoraReport2021.vue';
 import DoraReport2022 from './comics/DoraReport2022.vue';
@@ -8,6 +9,7 @@ const comicList = [
   'intro',
   'supply_chain_vulnerabilities',
   'dora_report_2021',
+  'software_architecture_overview',
   // 'dora_report_2022',
 ]
 const comicNumber = getComicNumber(comicList);
@@ -15,12 +17,13 @@ const comicNumber = getComicNumber(comicList);
 
 <template>
   <main>
-    <h1>DevOpsCon Sydney, 2022</h1>
+    <h1>Software Dev Resources</h1>
     <IntroComic v-if="comicNumber == comicList.indexOf('intro')" :comicList="comicList" />
     <SupplyChainVulnerabilities :comicList="comicList"
       v-else-if="comicNumber == comicList.indexOf('supply_chain_vulnerabilities')" />
     <DoraReport2021 :comicList="comicList" v-else-if="comicNumber == comicList.indexOf('dora_report_2021')" />
     <DoraReport2022 :comicList="comicList" v-else-if="comicNumber == comicList.indexOf('dora_report_2022')" />
+    <SoftwareArchitectureOverview :comicList="comicList" v-else-if="comicNumber == comicList.indexOf('software_architecture_overview')" />
     <div v-else>Sorry, no comic at this address.</div>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  assetsInclude: ['**/*.md'],
   base: './',
 })
